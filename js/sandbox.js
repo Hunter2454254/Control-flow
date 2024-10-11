94c6c6f59d66d5cf349cbf8906c980bbd55b972f
Hunter <126hhaynes@frhsd.com>
2024-10-11 13:37:36 -0400

    for loops

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,14 +1,14 @@
- for loops
+//  for loops
 
-for(let i = 0; i < 5; i++){
-    console.log('in loop:', i);
-}
+// for(let i = 0; i < 5; i++){
+//     console.log('in loop:', i);
+// }
 
-console.log('loop finished');
+// console.log('loop finished');
 
-// const names = ['shaun', 'mario', 'luigi'];
+const names = ['shaun', 'mario', 'luigi'];
 
-// for (let i = 0; i< names.length; i++);
-//     // console.log(names[i]);
-//     let html = `<div>${names[i]}</div>`;
-//     console.log(html);
+for (let i = 0; i< names.length; i++);
+    // console.log(names[i]);
+    let html = `<div>${names[i]}</div>`;
+    console.log(html); 
