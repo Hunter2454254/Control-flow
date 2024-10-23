c2043025100fc012cd8884c77117b0c4d548c49c
Hunter2454254 <126Hhaynes@frhsd.com>
2024-10-23 14:01:49 -0400

    All three cycles

--- js/sandbox.js
+++ js/sandbox.js
@@ -4,12 +4,29 @@ const d = new Date();
 let hour = d.getHours();
 
 console.log(hour);
+hour = 111;
+
 
 if (hour < 12) {
-// Morning
+    // Morning
+    document.getElementById("greeting").innerText = "Good Morning";
+    document.getElementById("images").src = "img/good.avif";
+
 } else if (hour < 17) {
-// Afternoon
-}
+    // Afternoon
+    document.getElementById("greeting").innerText = "Good Afternoon";
+    document.getElementById("images").src = "img/images.jpg";
+}  
 else {
-// Night
+    // Night
+    document.getElementById("greeting").innerText = "Good Night";
+    document.getElementById("images").src = "img/images (1).jpg";
+
+
 }
+
+
+
+//  let greetingElement = document.getElementById("Greeting");
+// console.log(greetingElement)
+
